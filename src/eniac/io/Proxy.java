1f428c0fb06a35279fc7469e37ae73ca859d8a42
Till Zoppke <kinnla@gmail.com>
2013-03-30 01:07:28 +0100

    GPL License

--- src/eniac/io/Proxy.java
+++ src/eniac/io/Proxy.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2003-2005, 2013 Till Zoppke.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Till Zoppke - initial API and implementation
+ ******************************************************************************/
 /*
  * Created on 11.02.2004
  */
