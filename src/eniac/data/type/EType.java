79c6f3096af3715e900597f3cb30ea38f1b565fa
Till Zoppke <kinnla@gmail.com>
2013-06-23 20:28:44 +0200

    formatting and commenting etypes

--- src/eniac/data/type/EType.java
+++ src/eniac/data/type/EType.java
@@ -23,7 +23,350 @@ import eniac.util.StringConverter;
 
 public enum EType {
 
-	CONFIGURATION_3, CONFIGURATION_4, CONFIGURATION_8, CONFIGURATION_12, CONFIGURATION_16, CONFIGURATION_20, ACCUMULATOR_UNIT, CYCLING_UNIT, INITIATING_UNIT, CONSTANT_TRANSMITTER_1_UNIT, CONSTANT_TRANSMITTER_2_UNIT, UPPER_TRUNK_2, UPPER_TRUNK_4, LOWER_TRUNK_3, LOWER_TRUNK_4, TRAY_2, TRAY_3, TRAY_4, BLINKEN_LIGHTS, ACCU_HEATERS, HEATERS, HEATERS_01, STEP_BUTTON, GO_BUTTON, CLEAR_BUTTON, CYCLE_COUNTER_CLEAR, ITERATION_SWITCH, SIGNIFICIANT_FIGURES_SWITCH, OPERATION_SWITCH, REPEAT_SWITCH, CIPHER, BLINKEN_NUMBER_SWITCH, BLINKEN_SIGN_SWITCH, CYCLING_LIGHTS, GO_LIGHTS, DIGIT_CONNECTOR, PROGRAM_CONNECTOR, INTER_CONNECTOR, DIGIT_CONNECTOR_CROSS, PROGRAM_CONNECTOR_PAIR, BLEND_8, BLEND_16, BLEND_A_10, CYCLING_SYMBOL, INITIATING_SYMBOL, CONSTANT_TRANSMITTER_1_SYMBOL, CONSTANT_TRANSMITTER_2_SYMBOL, CONSTANT_SELECTOR_SWITCH_AB, CONSTANT_SELECTOR_SWITCH_CD, CONSTANT_SELECTOR_SWITCH_EF, CONSTANT_SELECTOR_SWITCH_GH, CONSTANT_SELECTOR_SWITCH_JK, CONSTANT_SWITCH, INITIATING_IMAGE, FREQUENCY_SLIDER, BENCHMARK, CYCLE_COUNTER, XOR_IMAGE, CONSTANT_SIGN_TOGGLE_JL, CONSTANT_SIGN_TOGGLE_JR, CONSTANT_SIGN_TOGGLE_KL, CONSTANT_SIGN_TOGGLE_KR, CONSTANT_2_LIGHTS, CONSTANT_BLINKEN_CIPHER, CONSTANT_BLINKEN_SIGN, CONSTANT_TRANSMITTION_CIPHER, CONSTANT_TRANSMITTION_SIGN, CONSTANT_TRANSMITTION_LIGHTS;
+	/**
+	 * A configuration of 3 units
+	 */
+	CONFIGURATION_3,
+
+	/**
+	 * A configuration of 4 units
+	 */
+	CONFIGURATION_4,
+
+	/**
+	 * A configuration of 8 units
+	 */
+	CONFIGURATION_8,
+
+	/**
+	 * A configuration of 12 units
+	 */
+	CONFIGURATION_12,
+
+	/**
+	 * A configuration of 16 units
+	 */
+	CONFIGURATION_16,
+
+	/**
+	 * A configuration of 20 units
+	 */
+	CONFIGURATION_20,
+
+	/**
+	 * the accumulator
+	 */
+	ACCUMULATOR_UNIT,
+
+	/**
+	 * the cycling unit
+	 */
+	CYCLING_UNIT,
+
+	/**
+	 * the initiating unit
+	 */
+	INITIATING_UNIT,
+
+	/**
+	 * the 1st constant transmitter unit
+	 */
+	CONSTANT_TRANSMITTER_1_UNIT,
+
+	/**
+	 * the 2nd constant transmitter unit
+	 */
+	CONSTANT_TRANSMITTER_2_UNIT,
+
+	/**
+	 * an upper trunk which is 2 units width
+	 */
+	UPPER_TRUNK_2,
+
+	/**
+	 * an upper trunk which is 4 units width
+	 */
+	UPPER_TRUNK_4,
+
+	/**
+	 * a lower trunk which is 3 units width
+	 */
+	LOWER_TRUNK_3,
+
+	/**
+	 * a lower trunk which is 4 units width
+	 */
+	LOWER_TRUNK_4,
+
+	/**
+	 * a tray (part of a trunk) which is 2 units width
+	 */
+	TRAY_2,
+
+	/**
+	 * a tray (part of a trunk) which is 3 units width
+	 */
+	TRAY_3,
+
+	/**
+	 * a tray (part of a trunk) which is 4 units width
+	 */
+	TRAY_4,
+
+	/**
+	 * a blinken lights of an accumulator
+	 */
+	BLINKEN_LIGHTS,
+
+	/**
+	 * the heaters switch of an accumulator (may display the accumulator icon)
+	 */
+	ACCU_HEATERS,
+
+	/**
+	 * a standard heaters switch
+	 */
+	HEATERS,
+
+	/**
+	 * a heaters switch that displays 0 - 1
+	 */
+	HEATERS_01,
+
+	/**
+	 * the step button of the cycling unit
+	 */
+	STEP_BUTTON,
+
+	/**
+	 * the go button of the initiating unit
+	 */
+	GO_BUTTON,
+
+	/**
+	 * the clear button of the initiating unit
+	 */
+	CLEAR_BUTTON,
+
+	/**
+	 * the clear button for the cycle counter
+	 */
+	CYCLE_COUNTER_CLEAR,
+
+	/**
+	 * the iteration switch of the cycling unit
+	 */
+	ITERATION_SWITCH,
+
+	/**
+	 * the significant figures switch of an accumulator
+	 */
+	SIGNIFICIANT_FIGURES_SWITCH,
+
+	/**
+	 * the operations switch of an accumulator
+	 */
+	OPERATION_SWITCH,
+
+	/**
+	 * the repeat switch of an accumulator
+	 */
+	REPEAT_SWITCH,
+
+	/**
+	 * a cipher of the cycle counter at the cycling unit
+	 */
+	CIPHER,
+
+	/**
+	 * a digit of the blinken lights
+	 */
+	BLINKEN_NUMBER_SWITCH,
+
+	/**
+	 * the sign of the blinken lights
+	 */
+	BLINKEN_SIGN_SWITCH,
+
+	/**
+	 * the cycling lights -- display the pulse
+	 */
+	CYCLING_LIGHTS,
+
+	/**
+	 * the go lights of the initiating unit
+	 */
+	GO_LIGHTS,
+
+	/**
+	 * a digit connector (10 digits + sign + carryover)
+	 */
+	DIGIT_CONNECTOR,
+
+	/**
+	 * a program connector (1 pulse)
+	 */
+	PROGRAM_CONNECTOR,
+
+	/**
+	 * an inter-connector (can connect 2 accumulator units)
+	 */
+	INTER_CONNECTOR,
+
+	/**
+	 * a digit connector in horizontal layout (e.g. at the edges of a tray)
+	 */
+	DIGIT_CONNECTOR_CROSS,
+
+	/**
+	 * a pair of program connectors (in + out)
+	 */
+	PROGRAM_CONNECTOR_PAIR,
+
+	/**
+	 * a cover, without functionality (accumulator, between heaters and
+	 * significant figures switch)
+	 */
+	BLEND_8,
+
+	/**
+	 * a cover, without functionality (accumulator, above the repeat switches)
+	 */
+	BLEND_16,
+
+	/**
+	 * a cover, without functionality (constant transmitter panel)
+	 */
+	BLEND_A_10,
+
+	/**
+	 * the symbol for the cycling unit
+	 */
+	CYCLING_SYMBOL,
+
+	/**
+	 * the symbol for the initiating unit
+	 */
+	INITIATING_SYMBOL,
+
+	/**
+	 * the symbol for the constant transmitter unit 1
+	 */
+	CONSTANT_TRANSMITTER_1_SYMBOL,
+
+	/**
+	 * the symbol for the constant transmitter unit 2
+	 */
+	CONSTANT_TRANSMITTER_2_SYMBOL,
+
+	/**
+	 * the constant selector switch for A+B
+	 */
+	CONSTANT_SELECTOR_SWITCH_AB,
+
+	/**
+	 * the constant selector switch for C+D
+	 */
+	CONSTANT_SELECTOR_SWITCH_CD,
+
+	/**
+	 * the constant selector switch for E+F
+	 */
+	CONSTANT_SELECTOR_SWITCH_EF,
+
+	/**
+	 * the constant selector switch for G+H
+	 */
+	CONSTANT_SELECTOR_SWITCH_GH,
+
+	/**
+	 * the constant selector switch for J+K
+	 */
+	CONSTANT_SELECTOR_SWITCH_JK,
+
+	/**
+	 * a number switch at the constant transmitter unit 2
+	 */
+	CONSTANT_SWITCH,
+
+	/**
+	 * the image displayed at the initiating unit
+	 */
+	INITIATING_IMAGE,
+
+	/**
+	 * the frequency slider (cycling unit)
+	 */
+	FREQUENCY_SLIDER,
+
+	/**
+	 * the benchmark, displaying the number of addition cycles per second
+	 */
+	BENCHMARK,
+
+	/**
+	 * the addition cycle counter
+	 */
+	CYCLE_COUNTER,
+
+	/**
+	 * image to be used in the overview panel, as marker for the currently
+	 * visible area
+	 */
+	XOR_IMAGE,
+
+	/**
+	 * toggle switch for the constant transmitter 2, row J, left
+	 */
+	CONSTANT_SIGN_TOGGLE_JL,
+
+	/**
+	 * toggle switch for the constant transmitter 2, row J, right
+	 */
+	CONSTANT_SIGN_TOGGLE_JR,
+
+	/**
+	 * toggle switch for the constant transmitter 2, row K, left
+	 */
+	CONSTANT_SIGN_TOGGLE_KL,
+
+	/**
+	 * toggle switch for the constant transmitter 2, row K, right
+	 */
+	CONSTANT_SIGN_TOGGLE_KR,
+
+	/**
+	 * the numbers display at the constant transmitter 2
+	 */
+	CONSTANT_2_LIGHTS,
+
+	/**
+	 * a decimal cipher of the constant transmitter 2 lights
+	 */
+	CONSTANT_BLINKEN_CIPHER,
+
+	/**
+	 * the +/- sign for a number at the constant transmitter 2 lights
+	 */
+	CONSTANT_BLINKEN_SIGN,
+
+	/**
+	 * a cipher displayed at constant transmitter 1, to be shown during a
+	 * transmission
+	 */
+	CONSTANT_TRANSMITTION_CIPHER,
+
+	/**
+	 * a =/- sign displayed at constant transmitter 1, to be shown during a
+	 * transmission
+	 */
+	CONSTANT_TRANSMITTION_SIGN,
+
+	/**
+	 * the lights at constant transmitter 1, will display number and sign during
+	 * a transmission
+	 */
+	CONSTANT_TRANSMITTION_LIGHTS;
 
 	public enum Tag {
 		NAME, TYPE, MODEL, VIEW, CODE, CODES, VALUE;
